3b9aa9e97be5381e015bff8b9c6f93dd6cbcfeda
Ash Kyd <ash@kyd.com.au>
2019-07-06 13:53:51 +0200

    Desktop: add throttle on resize & fix handler

--- src/components/desktop/index.js
+++ src/components/desktop/index.js
@@ -1,17 +1,24 @@
 import { h, render, Component } from "preact";
+import throttle from "lodash/throttle";
 import "./style.css";
 
 class Desktop {
+  constructor() {
+    this.updateFullscreen = throttle(this.updateFullscreen, 100).bind(this);
+  }
   componentDidMount() {
-    if (this.props.fullscreen) {
-      window.addEventListener("resize", this.updateFullscreen);
-      this.updateFullscreen();
-    }
+    window.addEventListener("resize", this.updateFullscreen);
+    this.updateFullscreen();
+  }
+  compontDidUpdate() {
+    this.updateFullscreen();
   }
   componentWillUnmount() {
     window.removeEventListener("resize", this.updateFullscreen);
   }
   updateFullscreen() {
+    console.log(this);
+    console.log("resize", Date.now());
     this.el.style.height = window.innerHeight + "px";
     this.el.style.width = window.innerWidth + "px";
   }
