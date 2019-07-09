93025b15fd2c084c882fb99b6991e9f1b89f9129
Ash Kyd <ash@kyd.com.au>
2019-07-09 22:54:59 +0200

    remove console.log

--- src/components/html/index.js
+++ src/components/html/index.js
@@ -2,10 +2,6 @@ import { h, render, Component } from "preact";
 
 class HTML extends Component {
   shouldComponentUpdate(previousState) {
-    console.log({
-      previousLength: previousState.html && previousState.html.length,
-      current: this.props.html && this.props.html.length
-    });
     return previousState.html !== this.props.html;
   }
   render(props) {
