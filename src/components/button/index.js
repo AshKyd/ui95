7fb0ae4939205379cc2263c5e60dc1f5c3f608c8
Ash Kyd <ash@kyd.com.au>
2021-05-13 19:25:32 +1000

    Stylistic refactor to be more Reacty

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,13 +2,15 @@ import { h, render, Component } from "preact";
 import { getClasses } from "../../util";
 import "./style.css";
 
-function Button(props) {
-  return h(props.type || "button", {
-    ...props,
-    ref: props.fwdRef,
-    className: getClasses("button", props.classNames),
-    "aria-pressed": (props.classNames || "").includes("active")
-  });
+function Button({ type: Element = "button", fwdRef, classNames, ...props }) {
+  return (
+    <Element
+      {...props}
+      ref={fwdRef}
+      className={getClasses("button", classNames)}
+      aria-pressed={(classNames || "").includes("active")}
+    />
+  );
 }
 
 export default Button;
